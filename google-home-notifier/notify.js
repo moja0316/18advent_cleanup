f52463e13f2f1f5c0771fce51e9f10d332eb9209
Takuma <takuma@DESKTOP-4F2IIIP.localdomain>
2018-11-26 00:54:17 +0900

    add google-home-notifier

---
+++ google-home-notifier/notify.js
@@ -0,0 +1,15 @@
+var googlehome = require('./google-home-notifier');
+var language = 'ja'; // if not set 'us' language will be used
+
+//googlehome.device('Google Home', language); // Change to your Google Home name
+// or if you know your Google Home IP
+googlehome.ip('192.168.1.1', language);
+
+var msg = 'こんにちは。世界。';
+if(process.argv.length > 2){
+  msg = process.argv[2];
+}
+
+googlehome.notify(msg, function(res) {
+  console.log(res);
+});
